79c663a9f2899252613135598f1b61a4e14e83c2
Denis López <denuxs@gmail.com>
2025-08-26 14:40:44 -0600

    ♻️  save account with groups and permissions

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -27,6 +27,13 @@ class PermissionSerializer(serializers.ModelSerializer):
 
 class AccountSerializer(serializers.ModelSerializer):
 
+    groups = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Group.objects.all(), required=False, allow_empty=True
+    )
+    user_permissions = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Permission.objects.all(), required=False, allow_empty=True
+    )
+
     class Meta:
         model = User
         fields = (
@@ -41,21 +48,47 @@ class AccountSerializer(serializers.ModelSerializer):
             "photo",
             "phone",
             "role",
+            "groups",
+            "user_permissions",
         )
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
-
-    def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
-        user = super().update(instance, validated_data)
+        groups = validated_data.pop("groups", [])
+        user_permissions = validated_data.pop("user_permissions", [])
 
+        user = User.objects.create(**validated_data)
         if password:
             user.set_password(password)
             user.save()
+
+        user.groups.set(groups)
+        user.user_permissions.set(user_permissions)
+        user.save()
         return user
+
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(**validated_data)
+    #     return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        groups = validated_data.pop("groups", [])
+        user_permissions = validated_data.pop("user_permissions", [])
+
+        instance = super().update(instance, validated_data)
+
+        if password:
+            instance.set_password(password)
+
+        if groups:
+            instance.groups.set(groups)
+
+        if user_permissions:
+            instance.user_permissions.set(user_permissions)
+        instance.save()
+        return instance
 
 
 class AccountReadSerializer(serializers.ModelSerializer):
